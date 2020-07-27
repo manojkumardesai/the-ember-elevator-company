a0e9a531b3600f5d957f06b2ce2a8e4cafeee35b
manojkumardesai <manojkumardbe@gmail.com>
2020-07-27 21:14:23 +0530

    removed repeting code

--- app/components/lifts-view.js
+++ app/components/lifts-view.js
@@ -8,143 +8,40 @@ export default class LiftsViewComponent extends Component {
         'lift-three': 0
     };
     @action
-    upstart1() {
-        let [nearestLift] = this.getNearestLift(1);;
-        let rectElement = document.getElementById(nearestLift);
-        let currentClass = rectElement.getAttribute("class");
+    goUp(floor) {
+        const [nearestLift] = this.getNearestLift(floor);;
+        const rectElement = document.getElementById(nearestLift);
+        const currentClass = rectElement.getAttribute("class");
         if (currentClass) {
             rectElement.classList.remove(`${currentClass}`);
-            rectElement.classList.add('upAnimation1');
+            rectElement.classList.add('upAnimation' + floor);
         } else {
-            rectElement.classList.add('upAnimation1');
+            rectElement.classList.add('upAnimation1' + floor);
         }
-        console.log(nearestLift);
-        this.liftTrack[nearestLift] = 1;
+        this.liftTrack[nearestLift] = floor;
     }
     @action
-    upstart2() {
-        let [nearestLift] = this.getNearestLift(2);;
-        let rectElement = document.getElementById(nearestLift);
-        let currentClass = rectElement.getAttribute("class");
+    goDown(floor) {
+        const [nearestLift] = this.getNearestLift(floor);;
+        const rectElement = document.getElementById(nearestLift);
+        const currentClass = rectElement.getAttribute("class");
         if (currentClass) {
             rectElement.classList.remove(`${currentClass}`);
-            rectElement.classList.add('upAnimation2');
-
+            rectElement.classList.add('downAnimation' + floor);
         } else {
-            rectElement.classList.add('upAnimation2');
+            rectElement.classList.add('downAnimation' + floor);
         }
-        console.log(nearestLift);
-        this.liftTrack[nearestLift] = 2;
-    }
-    @action
-    upstart3() {
-        let [nearestLift] = this.getNearestLift(3);
-        let rectElement = document.getElementById(nearestLift);
-        let currentClass = rectElement.getAttribute("class");
-        if (currentClass) {
-            rectElement.classList.remove(`${currentClass}`);
-            rectElement.classList.add('upAnimation3');
-
-        } else {
-            rectElement.classList.add('upAnimation3');
-        }
-        console.log(nearestLift);
-        this.liftTrack[nearestLift] = 3;
-    }
-    @action
-    upstart4() {
-        let [nearestLift] = this.getNearestLift(4);
-        let rectElement = document.getElementById(nearestLift);
-        let currentClass = rectElement.getAttribute("class");
-        if (currentClass) {
-            rectElement.classList.remove(`${currentClass}`);
-            rectElement.classList.add('upAnimation4');
-
-        } else {
-            rectElement.classList.add('upAnimation4');
-        }
-        console.log(nearestLift);
-        this.liftTrack[nearestLift] = 4;
-    }
-    @action
-    downstart1() {
-        let [nearestLift] = this.getNearestLift(1);
-        let rectElement = document.getElementById(nearestLift);
-        let currentClass = rectElement.getAttribute("class");
-        if (currentClass) {
-            rectElement.classList.remove(`${currentClass}`);
-            rectElement.classList.add('downAnimation1');
-
-        } else {
-            rectElement.classList.add('downAnimation1');
-        }
-        console.log(nearestLift);
-        this.liftTrack[nearestLift] = 1;
-    }
-    @action
-    downstart2() {
-        let [nearestLift] = this.getNearestLift(2);
-        let rectElement = document.getElementById(nearestLift);
-        let currentClass = rectElement.getAttribute("class");
-        if (currentClass) {
-            rectElement.classList.remove(`${currentClass}`);
-            rectElement.classList.add('downAnimation2');
-
-        } else {
-            rectElement.classList.add('downAnimation2');
-        }
-        console.log(nearestLift);
-        this.liftTrack[nearestLift] = 2;
-    }
-    @action
-    downstart3() {
-        let [nearestLift] = this.getNearestLift(3);
-        let rectElement = document.getElementById(nearestLift);
-        let currentClass = rectElement.getAttribute("class");
-        if (currentClass) {
-            rectElement.classList.remove(`${currentClass}`);
-            rectElement.classList.add('downAnimation3');
-
-        } else {
-            rectElement.classList.add('downAnimation3');
-        }
-        console.log(nearestLift);
-        this.liftTrack[nearestLift] = 3;
-    }
-    @action
-    downstart4() {
-        let [nearestLift] = this.getNearestLift(4);
-        let rectElement = document.getElementById(nearestLift);
-        let currentClass = rectElement.getAttribute("class");
-        if (currentClass) {
-            rectElement.classList.remove(`${currentClass}`);
-            rectElement.classList.add('downAnimation4');
-
-        } else {
-            rectElement.classList.add('downAnimation4');
-        }
-        console.log(nearestLift);
-        this.liftTrack[nearestLift] = 4;
+        this.liftTrack[nearestLift] = floor;
     }
     @action
     zero() {
-        let [nearestLift] = this.getNearestLift(0);
-        let rectElement = document.getElementById(nearestLift);
-        let currentClass = rectElement.getAttribute("class");
+        const [nearestLift] = this.getNearestLift(0);
+        const rectElement = document.getElementById(nearestLift);
+        const currentClass = rectElement.getAttribute("class");
         rectElement.classList.remove(`${currentClass}`);
         rectElement.classList.add('zeroUp');
         console.log(nearestLift);
         this.liftTrack[nearestLift] = 0;
-    }
-    @action
-    downstart5() {
-        let [nearestLift] = this.getNearestLift(5);
-        let rectElement = document.getElementById(nearestLift);
-        let currentClass = rectElement.getAttribute("class");
-        rectElement.classList.remove(`${currentClass}`);
-        rectElement.classList.add('downAnimation5');
-        console.log(nearestLift);
-        this.liftTrack[nearestLift] = 5;
     }
 
     getNearestLift(currentFloor) {
